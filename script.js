01078f45044ab87be0701d85b89140621786a286
Thanh Toàn <121237452+LockMan04@users.noreply.github.com>
2024-11-03 07:14:27 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+(function(_0x246bcb, _0x2a8cba) {
+    var _0x56d5c4 = _0x3abb,
+        _0x38ab2f = _0x246bcb();
+    while (true) {
+        try {
+            var _0x48480b = -parseInt(_0x56d5c4(0xc2)) / 1 + -parseInt(_0x56d5c4(0xcc)) / 2 + -parseInt(_0x56d5c4(0xbd)) / 3 + -parseInt(_0x56d5c4(0xbf)) / 4 + parseInt(_0x56d5c4(0xc3)) / 5 + parseInt(_0x56d5c4(0xc9)) / 6 + parseInt(_0x56d5c4(0xbe)) / 7 * (parseInt(_0x56d5c4(0xc5)) / 8);
+            if (_0x48480b === _0x2a8cba) break;
+            else _0x38ab2f.push(_0x38ab2f.shift());
+        } catch (_0x352195) {
+            _0x38ab2f.push(_0x38ab2f.shift());
+        }
+    }
+}(_0x1233, 0x77a4f));
+
+(function() {
+    var _0x3bf178 = _0x3abb,
+        _0x13b787 = document[_0x3bf178(0xcb)](_0x3bf178(0xc7));
+    let _0x5d7400 = document.querySelectorAll(_0x3bf178(0xc0));
+    _0x13b787.forEach(_0x3b3637 => _0x3b3637[_0x3bf178(0xcb)](_0x3bf178(0xc8))[4].setAttribute('checked', _0x3bf178(0xca)));
+    _0x13b787[_0x13b787.length - 2].querySelector(_0x3bf178(0xc8))[2].setAttribute('checked', _0x3bf178(0xca));
+    _0x13b787[_0x13b787.length - 1].querySelector(_0x3bf178(0xc8))[2].setAttribute(_0x3bf178(0xca), 'checked');
+    _0x5d7400.forEach(_0x1384e0 => _0x1384e0.value = _0x3bf178(0xcd));
+    document.querySelector(_0x3bf178(0xc1)).click();
+}());
+
+function _0x3abb(_0x47fb29, _0x2c7358) {
+    var _0x12330b = _0x1233();
+    return _0x3abb = function(_0x3abb98, _0x31d6b3) {
+        _0x3abb98 = _0x3abb98 - 0xbc;
+        var _0x24824a = _0x12330b[_0x3abb98];
+        return _0x24824a;
+    }, _0x3abb(_0x47fb29, _0x2c7358);
+}
+
+function _0x1233() {
+    var _0x13c3b5 = [
+        '885866lrIZDJ', 'Không có ý kiến', 'length', 'setAttribute', '430095ThNnci', '14zxvxzh',
+        '28456sptGsW', '.input-ykien', 'input[type=submit]', '765094ZiVbay', '3606480nhzoji',
+        'click', '2004040UivAQh', 'forEach', '.group-cautraloi', 'input[type=radio]',
+        '3757578NiGZZL', 'checked', 'querySelectorAll'
+    ];
+    _0x1233 = function() {
+        return _0x13c3b5;
+    };
+    return _0x1233();
+}
